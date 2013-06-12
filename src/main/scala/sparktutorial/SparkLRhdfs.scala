07e51dde4f41b2da8a94bb4dcc13ffb961aed200
Jay Yonamine <jay.yonamine@gmail.com>
2013-06-12 10:20:10 -0500

    Update SparkLRhdfs.scala

--- src/main/scala/sparktutorial/SparkLRhdfs.scala
+++ src/main/scala/sparktutorial/SparkLRhdfs.scala
@@ -34,7 +34,7 @@ object SparkLRhdfs {
     }
     
     val sc = new SparkContext("local", "SparkLRhdfs", "/home/jayyonamine/devel/spark", List("target/scala-2.9.2/spark-tutorial_2.9.2-0.1.jar"))
-    val data = sc.textFile("hdfs://ec2-54-226-253-122.compute-1.amazonaws.com:9000/data.tab").map(readPoints).cache()
+    val data = sc.textFile("hdfs://ec2-54-226-253-122.compute-1.amazonaws.com:9000/data.tab").map(readPoint).cache()
 
     // Initialize w to a random value
     var w = Vector(D, _ => 2 * rand.nextDouble - 1)
