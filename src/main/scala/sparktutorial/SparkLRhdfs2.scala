738d9ebdf1f93c93121f9cd3d7ec5f4f15a5e2c3
Jay Yonamine <jay.yonamine@gmail.com>
2013-06-13 09:58:51 -0400

    Update SparkLRhdfs2.scala

--- src/main/scala/sparktutorial/SparkLRhdfs2.scala
+++ src/main/scala/sparktutorial/SparkLRhdfs2.scala
@@ -1,3 +1,4 @@
+
 package sparktutorial
 import spark.SparkContext
 import SparkContext._
@@ -10,25 +11,29 @@ import spark._
 /**
  * Logistic regression based classification.
  */
-object SparkLRhdfs2 {
-  def main(args: Array[String]) {
-  println(args)
-  
-  var D = 4
+object SparkLRhdfs {
+  var D = 6
   var ITERATIONS = 3
   val rand = new Random(42)
 
-  case class DataPoint(x: Vector, y: Double)
+  	case class DataPoint(x: Vector, y: Double)
 
-  def readPoint(s: String) = {
+	def readPoint(s: String) = {
 		val mystring = s.split("\t")
 		val y = mystring.head.toDouble 
 		val x = Vector(mystring.tail.map(_.toDouble))
 		DataPoint(x,y)
 	}
+
+  def main(args: Array[String]) {
+  println("*************************************************************************************"+args.reduce(_+" "+_)+"******************************************************************************************************")
+    if (args.length == 0) {
+      System.err.println("Usage: SparkLR <master> [<slices>]")
+      System.exit(1)
+    }
     
-    val sc = new SparkContext("local", "SparkLRhdfs2", "/home/jayyonamine/devel/spark", List("target/scala-2.9.2/spark-tutorial_2.9.2-0.1.jar"))
-    val data = sc.textFile("hdfs://ec2-54-226-253-122.compute-1.amazonaws.com:9000/data.tab").map(readPoint).cache()
+    val sc = new SparkContext("local", "SparkLRhdfs", "/home/jayyonamine/devel/spark", List("target/scala-2.9.2/spark-tutorial_2.9.2-0.1.jar"))
+    val data = sc.textFile("hdfs://ec2-107-21-70-232.compute-1.amazonaws.com:9000/logit.data").map(readPoint).cache()
 
     // Initialize w to a random value
     var w = Vector(D, _ => 2 * rand.nextDouble - 1)
