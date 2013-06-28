cd153afa3bd6c53eab265d32d0866cf68ce45f86
Jay Yonamine <jay.yonamine@gmail.com>
2013-06-28 12:25:10 -0500

    Update wordcount2.scala

--- src/main/scala/sparktutorial/wordcount2.scala
+++ src/main/scala/sparktutorial/wordcount2.scala
@@ -5,10 +5,8 @@ import SparkContext._
 
 object WordCount2 extends App {
 
-  
   val sc = new SparkContext("spark://ec2-107-22-78-221.compute-1.amazonaws.com:7077", "Wordcount2", "/root/spark/", List("target/scala-2.9.2/spark-tutorial_2.9.2-0.1.jar"))
-  
-  val file = sc.textFile(hdfs://ec2-107-22-78-221.compute-1.amazonaws.com:9000/book.txt)
+  val file = sc.textFile("hdfs://ec2-107-22-78-221.compute-1.amazonaws.com:9000/book.txt")
   val counts = file.flatMap(line => line.split(" "))
     .map(word => (word, 1))
     .reduceByKey(_ + _)
