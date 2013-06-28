967e8b7b10812faff8077216fd5a0cc0dc6dcbf1
Jay Yonamine <jay.yonamine@gmail.com>
2013-06-28 12:59:04 -0500

    Update wordcount2.scala

--- src/main/scala/sparktutorial/wordcount2.scala
+++ src/main/scala/sparktutorial/wordcount2.scala
@@ -1,14 +1,59 @@
-package sparktutorial
 
+package sparktutorial
 import spark.SparkContext
 import SparkContext._
 
-object WordCount2 extends App {
+import java.util.Random
+import scala.math.exp
+import spark.util.Vector
+import spark._
 
-  val sc = new SparkContext("spark://ec2-107-22-78-221.compute-1.amazonaws.com:7077", "Wordcount2", "/root/spark/", List("target/scala-2.9.2/spark-tutorial_2.9.2-0.1.jar"))
-  val file = sc.textFile("hdfs://ec2-107-22-78-221.compute-1.amazonaws.com:9000/book.txt")
-  val counts = file.flatMap(line => line.split(" "))
-    .map(word => (word, 1))
-    .reduceByKey(_ + _)
-  println(counts)
+/**
+ * Logistic regression based classification.
+ */
+object asdf {
+  val N = 100000  // Number of data points
+  val D = 10  // Numer of dimensions
+  val R = 0.7  // Scaling factor
+  val ITERATIONS = 10
+  val rand = new Random(42)
+
+  case class DataPoint(x: Vector, y: Double)
+
+  def generateData = {
+    def generatePoint(i: Int) = {
+      val y = if(i % 2 == 0) -1 else 1
+      val x = Vector(D, _ => rand.nextGaussian + y * R)
+      DataPoint(x, y)
+    }
+    Array.tabulate(N)(generatePoint)
+  }
+
+  def main(args: Array[String]) {
+  println(args)
+    if (args.length == 0) {
+      System.err.println("Usage: SparkLR <master> [<slices>]")
+      System.exit(1)
+    }
+    
+    val sc = new SparkContext("local", "test", "~/spark", List("target/scala-2.9.2/spark-tutorial_2.9.2-0.1.jar"))
+    val points = sc.parallelize(generateData)
+
+    // Initialize w to a random value
+    var w = Vector(D, _ => 2 * rand.nextDouble - 1)
+    println("Initial w: " + w)
+
+    for (i <- 1 to ITERATIONS) {
+      System.err.println("\n\n\n\nStarting iteration " + i + "\n\n\n\n")
+      println("On iteration " + i)
+      val gradient = points.map { p =>
+        (1 / (1 + exp(-p.y * (w dot p.x))) - 1) * p.y * p.x
+      }.reduce(_ + _)
+      w -= gradient
+    }
+
+    println("Final w: " + w)
+    System.exit(0)
+  }
 }
+Status Developer Training Shop Blog About © 2013 GitHub, Inc. Terms Privacy Security Contact 
