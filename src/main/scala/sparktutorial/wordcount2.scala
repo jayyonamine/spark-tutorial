321bb6be8f592dc5ee03df67d37b46475cd33aad
Jay Yonamine <jay.yonamine@gmail.com>
2013-06-28 15:14:30 -0500

    Update wordcount2.scala

--- src/main/scala/sparktutorial/wordcount2.scala
+++ src/main/scala/sparktutorial/wordcount2.scala
@@ -4,7 +4,7 @@ import spark.SparkContext
 import SparkContext._
 import spark._
 
-object WordCount2 extends App {
+object WordCount2 {
  def main(args: Array[String]) {
   val sc = new SparkContext(args(1), "Wordcount2", "/root/spark/", List("target/scala-2.9.2/spark-tutorial_2.9.2-0.1.jar"))
   val file = sc.textFile(args(2)).cache()
