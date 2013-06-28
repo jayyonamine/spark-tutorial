2d79cf061bb4cc34b91e741bf152054542b87d57
Jay Yonamine <jay.yonamine@gmail.com>
2013-06-28 14:48:35 -0500

    Update wordcount2.scala

--- src/main/scala/sparktutorial/wordcount2.scala
+++ src/main/scala/sparktutorial/wordcount2.scala
@@ -1,57 +1,16 @@
 package sparktutorial
+
 import spark.SparkContext
 import SparkContext._
-
-import java.util.Random
-import scala.math.exp
-import spark.util.Vector
 import spark._
 
-/**
- * Logistic regression based classification.
- */
-object asdf {
-  val N = 100000  // Number of data points
-  val D = 10  // Numer of dimensions
-  val R = 0.7  // Scaling factor
-  val ITERATIONS = 10
-  val rand = new Random(42)
-
-  case class DataPoint(x: Vector, y: Double)
-
-  def generateData = {
-    def generatePoint(i: Int) = {
-      val y = if(i % 2 == 0) -1 else 1
-      val x = Vector(D, _ => rand.nextGaussian + y * R)
-      DataPoint(x, y)
-    }
-    Array.tabulate(N)(generatePoint)
-  }
-
-  def main(args: Array[String]) {
-  println(args)
-    if (args.length == 0) {
-      System.err.println("Usage: SparkLR <master> [<slices>]")
-      System.exit(1)
-    }
-    
-    val sc = new SparkContext("local", "asdf", "~/spark", List("target/scala-2.9.2/spark-tutorial_2.9.2-0.1.jar"))
-    val points = sc.parallelize(generateData)
-
-    // Initialize w to a random value
-    var w = Vector(D, _ => 2 * rand.nextDouble - 1)
-    println("Initial w: " + w)
-
-    for (i <- 1 to ITERATIONS) {
-      System.err.println("\n\n\n\nStarting iteration " + i + "\n\n\n\n")
-      println("On iteration " + i)
-      val gradient = points.map { p =>
-        (1 / (1 + exp(-p.y * (w dot p.x))) - 1) * p.y * p.x
-      }.reduce(_ + _)
-      w -= gradient
-    }
-
-    println("Final w: " + w)
-    System.exit(0)
+object WordCount2 extends App {
+ def main(args: Array[String]) {
+  val sc = new SparkContext(args(1), "Wordcount2", "/root/spark/", List("target/scala-2.9.2/spark-tutorial_2.9.2-0.1.jar"))
+  val file = sc.textFile(args(2)).cache()
+  val counts = file.flatMap(line => line.split(" "))
+    .map(word => (word, 1))
+    .reduceByKey(_ + _)
+  println(counts)
   }
 }
